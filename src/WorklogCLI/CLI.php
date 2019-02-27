0c8d4f8dcbdaa80b771ac6079cf3c63c58bd4065
Lindsay Bernath <lindsaybernath@Lindsays-MacBook-Pro.local>
2019-02-26 21:43:42 -0500

    allows passing keys to invoice2 command to return only some invoice data

--- src/WorklogCLI/CLI.php
+++ src/WorklogCLI/CLI.php
@@ -751,8 +751,16 @@ class CLI {
   }
   public static function op_invoice2() {
 
+    $section = CLI::$args[0] ?: null;
+    
     // build summary
     $yaml_data = WorklogSummary::summary_invoice2();
+    $subset = [];
+    foreach(CLI::$args as $arg) {
+      if (array_key_exists($arg, $yaml_data)) 
+        $subset[$arg] = $yaml_data[$arg];  
+    }
+    if (!empty($subset)) $yaml_data = $subset;
     $output = Output::formatted_yaml($yaml_data);
     CLI::out( $output );
 
